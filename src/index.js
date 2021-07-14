3fa4e50a8b4c10634d3be208611907e4202893c3
Manuja.kasun <manuja2zlato@gmail.com>
2021-07-14 19:49:43 +0530

    Components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello(){
+function Hello({library,message,number1,number2}){
   return(
-    <div>
-      <h1 id="heading"className="cool-text"> WElcome to Sri Lanka</h1>
-    </div>
+   <div>
+     <h1 id = "heading">Welcome to {library} !</h1>
+   <p>{message}</p>
+   Total = (number1+number2)
+   </div>
   );
   }
 ReactDOM.render(
-   <Hello/>,
+   <Hello library = "React" 
+   message = "Have Fun"
+   number1 = {20}
+   number2 = {10}/>,
     document.getElementById('root')
 );
 

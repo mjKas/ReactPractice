218c35b393f03e76aacf0d2780f5a628d49bca91
Manuja.kasun <manuja2zlato@gmail.com>
2021-07-14 20:51:09 +0530

    conditional (if else) short form

--- src/index.js
+++ src/index.js
@@ -2,27 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const CityList = [
-  {id:"1", name: "Kandy" ,user:"Joe"},
-  {id:"2", name: "Colombo", user:"Jane"},
-  {id:"3", name: "Gall", user: "Sarah"}
-];
-
-function App({cities}){
-  return (
-   <div>
-     {cities.map(city =>(
-       <div key={city.id}>
-         <h2>{city.name}</h2>
-         <p>Acessed by: {city.user}</p>
+function BeachResort({name}){
+  return(
+    <div>
+      <h1>Welcome to {name}!</h1>
     </div>
-     ))}
-   </div>
   );
 }
+
+function LakeResort({name}){
+  return(
+    <div>
+      <h1>Welcome to {name}</h1>
+    </div>
+  );
+}
+
+function App(props){
+  return(
+    <div>
+      /**if else short form */
+      {(props.season==="summer")?<BeachResort name="Welcome to Marble Beach Resort"/>:
+      (props.season==="winter")?<LakeResort name="Lake Gregory Resort"/>:<h1>"Come back on winter or summer"</h1>}
+    </div>
+  );
+  }
+  
+
   
 ReactDOM.render(
-    <App cities = { CityList} />,
+    <App season = "summer"/>, /*here in app season we pass what season if it is winter or summer */
     document.getElementById('root')
 );
 
